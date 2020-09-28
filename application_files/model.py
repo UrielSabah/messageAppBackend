d88d426eb5dd6d9e65c6533f1d9e10562a42a132
UrielSabah <50175717+UrielSabah@users.noreply.github.com>
2020-09-28 04:14:20 +0300

    Update model.py

--- application_files/model.py
+++ application_files/model.py
@@ -31,28 +31,3 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post('{self.sender}', '{self.subject}', '{self.open_message},'{self.date_posted}')"
 
-
-
-
-
-
-# class User(UserMixin, db.Model):
-#     """ User model """
-#
-#     __tablename__ = "users"
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(25), unique=True, nullable=False)
-#     hashed_pswd = db.Column(db.String(), nullable=False)
-#
-# from application_files import db
-#
-#
-# class Message(db.Model):
-#     # ID = KEY of a point
-#     id = db.Column(db.Integer, primary_key=True)
-#     # The point coordinates,represented as a String of length 5, it has to be unique , and it can be null
-#     text = db.Column(db.String(5), unique=True, nullable=False)
-#     xCoord = db.Column(db.Integer, nullable=False)
-#     yCoord = db.Column(db.Integer, nullable=False)
-#     def __repr__(self):
-#         return f"Point('{self.id}','{self.text}','{self.xCoord}',{self.yCoord}')"
